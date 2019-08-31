32d1865c2644a1af7d6a384009303868ad84ac0d
jvp2001 <thisisminecraftpetersen@gmail.com>
2019-08-31 10:10:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,17 @@ let package = Package(
 	name: "Lua",
 	dependencies: [
         .package(url: "https://github.com/DavidSkrundz/CLua.git", .branch("master"))
-	]
+	],
+	targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Lua",
+            dependencies: []),
+        .testTarget(
+            name: "LuaTests",
+            dependencies: ["Lua"]),
+    ]
 )
 //let lib = Product(name)
 //let staticLibrary = Product(
